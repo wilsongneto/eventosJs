65eae34aa44e494a3a96a768d7393513ac9b996e
Wilson <109673935+wilsongneto@users.noreply.github.com>
2025-08-27 13:58:12 -0300

    js2

--- Scripts/script.js
+++ Scripts/script.js
@@ -1,1 +1,4 @@
-console.log(document.body);
+console.log(document.body);//Mostra no console o body do HTML
+
+console.log(document.body.childNodes); // Mostra no console um Array com todos os elementos usados no body do HTML
+
